c566818ebc2508d259dead1547bcc32477bd5c7e
lyra833 <alxg833@gmail.com>
2014-09-20 16:18:24 -0400

    Early coding

--- Computer Science/Assignments/hw2/List_linked_list.cpp
+++ Computer Science/Assignments/hw2/List_linked_list.cpp
@@ -5,11 +5,22 @@
 
 using namespace std;
 
-// hint: in your constructor, you might want to set the head to NULL
+List_linked_list::List_linked_list()
+{
+	head = NULL;
+	// hint: in your constructor, you might want to set the head to NULL
+}
 
 List_linked_list::~List_linked_list()
 {
 	// delete list by looping through and deleting each node
+	Card_Node * current = head;
+	while (current != NULL)
+	{
+		Card_Node temp = current->next;
+		delete current;
+		current = temp;
+	}
 }
 
 // copy constructor
@@ -104,5 +115,121 @@ void List_linked_list::print_list_int()
 	}
 }
 
+int List_linked_list::cards_in_hand()
+{
+	// returns the number of cards in the hand
+	int count = 0;
+	Card_Node * current = head;
+	while(current != NULL)
+	{
+		Card_Node *  temp = current->next;
+		current = temp; //I was going to put these in one line,
+		//but I don't remember C++'s order of precedence
+		count++;
+	}
+	return count;
+}
 
+void List_linked_list::make_empty()
+{
+	// empties the list; head should equal NULL
+	Card_Node * current = head;
+	while (current != NULL)
+        {
+                Card_Node temp = current->next;
+                delete current;
+                current = temp;
+        }
+	head = NULL;
+}
 
+// inserts a card at the head
+// Should allow insert into an empty list
+void List_linked_list::insert_at_head(Card c) //???
+{
+	Card_Node * node = new Card_Node();
+	node->card = c;
+	node->next = head; //If the list is empty and head is NULL,
+	//the new head would just point to a NULL as well.
+	head = &node;
+}
+
+// inserts a card at the tail (final node)
+// should allow insert into an empty list
+void List_linked_list::insert_at_tail(Card c)
+{
+	Card_Node * current = head;
+	while(current->next != NULL)
+	{
+		
+	}
+	//set current->next to the new Card_node
+}
+
+// inserts a card at an index such that
+// all cards following the index will be moved farther
+// down the list by one.
+// The index can be one after the tail of the list
+// I.e., you can insert at index 0 into an empty list,
+// and you can insert at index 4 in a list with 4 nodes.
+// In the first case, the node inserted would become the head
+// In the second case, the node inserted would be inserted
+// after the current tail.
+void List_linked_list::insert_at_index(Card c, int index)
+{
+
+}
+
+// replaces the card at index
+// A card at index must already exist
+void List_linked_list::replace_at_index(Card c, int index)
+{
+
+}
+
+// returns the card at index.
+// allowed to crash if index is not in the list
+Card List_linked_list::card_at(int index)
+{
+
+}
+
+// returns true if the card is in the list
+// returns false if the card is not in the list
+bool List_linked_list::has_card(Card c)
+{
+
+}
+
+// removes the card from the list
+// Returns true if the card was removed
+// Returns false if the card was not in the list
+bool List_linked_list::remove(Card c)
+{
+
+}
+
+// Removes the card from the head, and assigns the head
+// to head->next
+// Returns the card that was removed
+// Allowed to fail if list is empty
+Card List_linked_list::remove_from_head()
+{
+
+}
+
+// Removes the card from the tail
+// Returns the card that was removed
+// Allowed to fail if the list is empty
+Card List_linked_list::remove_from_tail()
+{
+
+}
+
+// Removes the card from index
+// Returns the card that was removed
+// Allowed to fail if index is beyond the end of the list
+Card List_linked_list::remove_from_index(int index)
+{
+
+}
