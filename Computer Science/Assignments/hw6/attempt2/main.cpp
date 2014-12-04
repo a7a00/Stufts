ef62b0f263da786f4052b9e7c9c435f575d52d1f
lyra833 <alxg833@gmail.com>
2014-12-03 20:06:15 -0500

    Haven't commited in a while...

---
+++ Computer Science/Assignments/hw6/attempt2/main.cpp
@@ -0,0 +1,23 @@
+/*
+ * main.cpp
+ *
+ */
+#include "songsearch.h"
+
+int main(int argc, char *argv[])
+{
+	string filename;
+	SongSearch songsearch;
+
+	if (argc == 2) { // if there is a filename on the command line
+		songsearch.read_lyrics(argv[1],true);
+	}
+	else { // use a pre-defined filename
+		filename = "rick_db.txt";
+		//filename = "lyrics_fulldb.txt";
+	        songsearch.read_lyrics(filename.c_str(),true);
+	}
+        songsearch.search();
+
+        return 0;
+}
--- Computer Science/Assignments/hw6/speedtests/boyermoore.cpp
+++ Computer Science/Assignments/hw6/speedtests/boyermoore.cpp
@@ -41,8 +41,11 @@ int find_match(string s, char c) //Finds the index of the last occurence of a ca
 
 int main()
 {
-	string searchstring = "Friendship is Magic!";
-	string pattern = "riendship is M";
+	//string searchstring = "Friendship is Magic!";
+	//string pattern = "riendship is M";
+
+	string searchstring = "You know I've been so lonely every night since you went away Would you let me down time any night is that the way you're made girl ? I keep telling myself it's alright even though I cry There's no question why how could I be happy when - My arms keep missing you who's been kissing you since you went away ? My arms keep missing you I keep wishing you'd come back today. You said you wouldn't hurt me any way and I believed in you We said we'd trust each other every day we'd never be untrue. But I keep thinking you're telling lies even though I try There's no question why how can I be happy when - My arms keep missing you who's been kissing you since you went away ? . . . You know I've been so lonely every night since you went away . . . My arms keep missing you who's been kissing you since you went away ? . . . My arms keep missing you who's been kissing you since you went away ? . . .";
+	string pattern = " time ";
 
 	int patternPointer = pattern.length() - 1;
 	int stringPointer = pattern.length() -1;
@@ -50,11 +53,17 @@ int main()
 	int extra = 0;
 	while(stringPointer < MAX)
 	{
+		if(stringPointer == 77)
+		{
+			cout << "765443!";
+		}
 		if(pattern[patternPointer] != searchstring[stringPointer])
 		{
 			extra = 0; //Reset this because we're at the end of the word.
+			//TODO This causes line 76 to have no effect. I'll bet you anything that's why this thing is breaking.
 			if(find_match(pattern, searchstring[stringPointer]) == -1) //If the string character isn't in the pattern
 			{
+				//extra = 0;
 				patternPointer = pattern.length() - 1; //Just shift the whole pattern over
 				stringPointer += pattern.length();
 				if(stringPointer >= MAX) break; //Prevents out of bounds segfaults
@@ -65,6 +74,7 @@ int main()
 				stringPointer += (pattern.length()-(find_match(pattern, searchstring[stringPointer])));
 				stringPointer -= 1;
 				stringPointer += extra;
+				//extra = 0;
 				if(stringPointer >= MAX) break; //Prevents out of bounds segfaults
 			}
 		}
